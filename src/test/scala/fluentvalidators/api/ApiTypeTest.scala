f0e96285fdc4e3ecd3ac7277de2e330140f8cd81
mucciolo <diego_mucciolo@hotmail.com>
2023-03-13 13:12:54 -0300

    added ApiTypeTest

---
+++ src/test/scala/fluentvalidators/api/ApiTypeTest.scala
@@ -0,0 +1,50 @@
+package com.mucciolo
+package fluentvalidators.api
+
+import fluentvalidators.api.Validator
+import fluentvalidators.api.syntax.*
+
+import cats.data.ValidatedNec
+import cats.implicits.catsSyntaxValidatedIdBinCompat0
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+
+class ApiTypeTest extends AnyWordSpec with should.Matchers {
+
+  "Validator" when {
+    "validatee type is A and B <: A" should {
+
+      class A
+      class B extends A
+      val validatorA: Validator[Any, A] = Validator.of[A].withErrorTypeOf[Any]
+
+      "be a validator of type B" in {
+        val validatorB: Validator[Any, B] = validatorA
+
+        validatorA should === (validatorB)
+      }
+
+      "preserve validated subclass type" in {
+        val b = new B
+        val validB: ValidatedNec[Any, B] = validatorA.validate(b)
+
+        validB should === (b.validNec)
+      }
+
+    }
+
+    "error type is F and F <: E" should {
+
+      class E
+      class F extends E
+
+      "be a validator with error type E" in {
+        val f: Validator[F, Any] = Validator.of[Any].withErrorTypeOf[F]
+        val e: Validator[E, Any] = f
+
+        f should === (e)
+      }
+    }
+  }
+
+}
