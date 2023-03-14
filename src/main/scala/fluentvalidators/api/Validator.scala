812d35f9bc180e865e52af354f49f863cdf7e2ad
mucciolo <diego_mucciolo@hotmail.com>
2023-03-14 20:05:53 -0300

    minimal Validator Scaladoc

--- src/main/scala/fluentvalidators/api/Validator.scala
+++ src/main/scala/fluentvalidators/api/Validator.scala
@@ -7,19 +7,32 @@ import cats.data.*
 
 trait Validator[+E, -A] {
 
+  /**
+    * Performs the validation of the given [[instance]] according to the rules of this validator.
+    */
   def validate[B <: A](instance: B): ValidatedNec[E, B]
 
+  /**
+    * Applies the given validators in order, short-circuiting the validation on the first failure.
+    */
   def seq[EE >: E, B <: A](
     headValidator: Validator[EE, B],
     tailValidators: Validator[EE, B]*
   ): Validator[EE, B]
 
+  /**
+    * Applies the given validators in parallel, accumulating their errors.
+    */
   def par[EE >: E, B <: A](
     firstValidator: Validator[EE, B],
     secondValidator: Validator[EE, B],
     tailValidators: Validator[EE, B]*
   ): Validator[EE, B]
 
+  /**
+    * A contramap in the validatee type and a map in the error type.
+    * @note Terminology borrowed from profunctors.
+    */
   def dimap[B, F](f: B => A, g: E => F): Validator[F, B]
 
 }
